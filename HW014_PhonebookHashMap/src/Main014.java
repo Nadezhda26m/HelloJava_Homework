a09c6aed3ecb753eaaacef1eb01c20239d73d363
Nadezhda26m <nadezhda-26m@mail.ru>
2023-02-23 19:50:15 +0300

    HW 014 key to String

--- HW014_PhonebookHashMap/src/Main014.java
+++ HW014_PhonebookHashMap/src/Main014.java
@@ -3,14 +3,17 @@
 что 1 человек может иметь несколько телефонов.
  */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main014 {
 
     public static void main(String[] args) {
-        // ["Ф", "И", "О", "дд.мм.гггг"] : ["тел1", "тел2"]
-        Map<String[], ArrayList<String>> phBook = new HashMap<>();
-        String[] key;
+        // "Ф-И-О-дд.мм.гггг" : ["тел1", "тел2"]
+        Map<String, ArrayList<String>> phBook = new HashMap<>();
+        String key;
         ArrayList<String> phones = new ArrayList<>(3);
 
         String stop = "";
@@ -20,22 +23,13 @@ public class Main014 {
             key = enterUserKey();
 
             addPhone = true;
-            System.out.println("ключи");
-            printPhonebook(phBook);
 
-            // if (phBook.containsKey(key)) {
-            if (checkContainsKey(key, phBook)) {
-                System.out.println("Контакт найден");
+            if (phBook.containsKey(key)) {
                 phones = phBook.get(key); // добавление нового номера к существующему контакту
-                System.out.println(phones);
             } else {
-                System.out.println("Контакт не найден");
-                System.out.println(phones);
                 phones.clear(); // добавление номеров нового контакта
-                System.out.println(phones);
             }
             while (addPhone) {
-                printKey(key);
                 System.out.print("Введите номер телефона: ");
                 newPhone = scan.nextLine();
                 if (phones.contains(newPhone)) {
@@ -53,45 +47,30 @@ public class Main014 {
                     addPhone = false;
                 }
             }
-            // key[0] = "Петрова";
-            // key[1] = "Светлана";
-            // key[2] = "Ивановна";
-            // key[3] = "23.02.1987";
-            // phones.add("+71234567890");
-            // phones.add("+71234767890");
+
             phBook.put(key, phones);
             System.out.println("Продолжить заполнение телефонной книги? ");
             stop = scan.nextLine();
             printPhonebook(phBook);
         }
         printPhonebook(phBook);
-
     }
 
     public static String[] userEnter = getUserEnter();
     public static Scanner scan = new Scanner(System.in);
 
-    public static boolean checkContainsKey(String[] key,
-                                            Map<String[], ArrayList<String>> phBook) {
-        for (var item : phBook.keySet()) {
-            if (Arrays.equals(key, item)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static String[] enterUserKey() {
-        String[] key = new String[4];
+    public static String enterUserKey() {
+        StringBuilder result = new StringBuilder();
         String newKey;
-        Scanner scan = new Scanner(System.in);
-
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.print(userEnter[i]);
             newKey = scan.nextLine();
-            key[i] = newKey;
+            result.append(newKey).append("-");
         }
-        return key;
+        System.out.print(userEnter[3]);
+        newKey = scan.nextLine();
+        result.append(newKey);
+        return result.toString();
     }
 
     public static String[] getUserEnter() {
@@ -103,15 +82,16 @@ public class Main014 {
         return userEnter;
     }
 
-    public static void printPhonebook(Map<String[], ArrayList<String>> phBook) {
+    public static void printPhonebook(Map<String, ArrayList<String>> phBook) {
         for (var item : phBook.entrySet()) {
             System.out.printf("%s : %s\n", printKey(item.getKey()),
                     printValue(item.getValue(), ", "));
         }
     }
 
-    public static String printKey(String[] array) {
+    public static String printKey(String str) {
         StringBuilder result = new StringBuilder();
+        String[] array = str.split("-");
         result.append(array[0]).append(" ");
         result.append(array[1]).append(" ");
         result.append(array[2]).append(", ");
